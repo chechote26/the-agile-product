aa898411561b72698de7d710db2f3e6cf2fbd5b1
Eze Tuero <chechote26@gmail.com>
2026-02-27 19:37:26 -0300

    Update Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const Blog = () => {
   useEffect(() => {
-    window.location.href = "https://theagileproduct.hashnode.dev";
+    window.location.href = "https://theagileproduct.substack.com";
   }, []);
 
   return null;
