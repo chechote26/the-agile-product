e7f5c85edf1b55dd484b89655f27954e536b4b21
chechote26 <chechote26@gmail.com>
2026-03-28 15:05:43 -0300

    fix3

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,24 +13,47 @@ import Laboratorio from "./pages/Laboratorio";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <LanguageProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/cursos" element={<Cursos />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/laboratorio" element={<Laboratorio />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </LanguageProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  // Sensor Global de Eventos
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      const target = (e.target as HTMLElement).closest('[data-id]');
+      if (target) {
+        const id = target.getAttribute('data-id');
+        // @ts-ignore
+        if (typeof window.gtag === 'function') {
+          // @ts-ignore
+          window.gtag('event', 'button_interaction', {
+            'button_name': id
+          });
+          console.log('✅ Global Tracking:', id);
+        }
+      }
+    };
+
+    document.addEventListener('click', handleClick, true);
+    return () => document.removeEventListener('click', handleClick, true);
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LanguageProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/cursos" element={<Cursos />} />
+              <Route path="*" element={<NotFound />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/laboratorio" element={<Laboratorio />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </LanguageProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
